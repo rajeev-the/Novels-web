6f4c20d112d2fa42e1318b9257550ab122e9c574
rajeev-the <146732212+rajeev-the@users.noreply.github.com>
2025-01-11 22:23:57 +0530

    Update urls.py

--- API/urls.py
+++ API/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('api/', include('bookAPI.urls')),
+    path('/', include('bookAPI.urls')),
      path('admin/', admin.site.urls),
 ]
