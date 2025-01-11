4056767b2ba32324b45ae9791da3c0bf39c70087
rajeev-the <146732212+rajeev-the@users.noreply.github.com>
2025-01-11 22:19:19 +0530

    Update settings.py

--- API/settings.py
+++ API/settings.py
@@ -137,8 +137,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-import os
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')  # Ensure this path is correct
+  # Ensure this path is correct
 
